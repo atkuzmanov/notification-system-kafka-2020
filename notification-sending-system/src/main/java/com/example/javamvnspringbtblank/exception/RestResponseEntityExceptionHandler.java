e58f35ad3f24257e6639826fa3be4253b7e290c0
atkuzmanov <atkuzmanov@gmail.com>
2020-12-17 16:35:45 +0200

    Work-In-Progress-WIP-2020-12-17-163545

---
+++ notification-sending-system/src/main/java/com/example/javamvnspringbtblank/exception/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,66 @@
+package com.example.javamvnspringbtblank.exception;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.convert.ConversionFailedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.lang.reflect.InvocationTargetException;
+
+@ControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    /**
+     * Catch all for any other exceptions.
+     */
+    @ExceptionHandler({Exception.class})
+    @ResponseBody
+    public ResponseEntity<?> handleAnyException(Exception e) {
+        return errorResponse(e, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    /**
+     * Handle failures commonly thrown from code.
+     */
+    @ExceptionHandler({InvocationTargetException.class, IllegalArgumentException.class, ClassCastException.class,
+            ConversionFailedException.class})
+    @ResponseBody
+    public ResponseEntity<?> handleMiscFailures(Throwable t) {
+        return errorResponse(t, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * Send a 409 Conflict in case of concurrent modification.
+     */
+//    @ExceptionHandler({ObjectOptimisticLockingFailureException.class, OptimisticLockingFailureException.class,
+//            DataIntegrityViolationException.class})
+//    @ResponseBody
+//    public ResponseEntity<?> handleConflict(Exception ex) {
+//        return errorResponse(ex, HttpStatus.CONFLICT);
+//    }
+
+    protected ResponseEntity<?> errorResponse(Throwable throwable, HttpStatus status) {
+        if (throwable != null) {
+//            return ResponseDetails.builder()
+//                    .status(status.value())
+//                    .responseMessage(throwable.getMessage())
+//                    .throwable(new Exception(throwable))
+//                    .headers(tracingResponseHeaders())
+//                    .entity();
+            new ResponseEntity<>(throwable.getMessage(), status);
+        } else {
+            return response(null, status);
+        }
+    }
+
+    protected <T> ResponseEntity<T> response(T body, HttpStatus status) {
+//        return new ResponseEntity<>(body, tracingResponseHeaders(), status);
+        return new ResponseEntity<>(body, status);
+    }
+}
